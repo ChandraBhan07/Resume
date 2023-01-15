49ead8dcd8c2d6c98b4c8e121a2d8d2cf6084950
Chandra Bhan Singh Parihar <chanduparihar1729@gmail.com>
2023-01-15 22:47:57 +0530

    Initial Commit

---
+++ layout 1/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    listStyleType: {
+      none: "none",
+      disc: "disc",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
